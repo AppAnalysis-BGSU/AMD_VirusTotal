96d83cd793da29c7e2de625dce456cff51bde643
shivapbhusal <civabhusal@gmail.com>
2018-04-03 21:44:19 -0400

    add calculation time

---
+++ executionTime/getExecutionTime.py
@@ -0,0 +1,19 @@
+'''
+Walks through the directory recursively and returns the list of files. 
+
+Example, python getFileList.py $folder. 
+
+If you want to store the result in a text file, python getFileList.py $folder >>amdFileList.py
+'''
+
+import os
+import sys
+
+directory=sys.argv[1]
+
+for root, dirs, files in os.walk(directory):
+    for myfile in files:
+        if myfile.endswith(".apk"):
+            print(os.path.join(root,myfile))
+
+
