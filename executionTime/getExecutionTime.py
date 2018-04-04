7bdb0eef3caf657a50a010fe7b1bfd8e68db0fbc
shivapbhusal <civabhusal@gmail.com>
2018-04-03 22:45:50 -0400

    add executionTime folder

--- executionTime/getExecutionTime.py
+++ executionTime/getExecutionTime.py
@@ -8,12 +8,25 @@ If you want to store the result in a text file, python getFileList.py $folder >>
 
 import os
 import sys
+import time as t
 
 directory=sys.argv[1]
+resultFile=sys.argv[2]
+execTimeFile=sys.argv[3]
 
 for root, dirs, files in os.walk(directory):
     for myfile in files:
         if myfile.endswith(".apk"):
-            print(os.path.join(root,myfile))
+            fullPath=os.path.join(root,myfile)
+            print('APK being scanned:'+fullPath)
+            startTime=t.time()
+            os.system('java -jar lockScreen.jar '+fullPath+' '+resultFile)
+            endTime=t.time()
+            execTime=endTime-startTime
+            createdFolder=fullPath.replace('.apk','')
+            os.system('rm -r '+createdFolder)
+            fileToStoreTime=open(execTimeFile,"a")
+            fileToStoreTime.write('\n'+myfile+','+str(execTime))
+            fileToStoreTime.close()
 
 
